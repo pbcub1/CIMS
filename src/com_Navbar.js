14dcbb5ffd0f3db9d876db706929205ac6d2e921
David Green <pbcub1.dg@gmail.com>
2018-08-10 15:57:49 -0600

    Nav Bar event handlers
    I added a link and added event handlers to it.

--- src/com_Navbar.js
+++ src/com_Navbar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+    processClick( e, trigger ){
+        e.preventDefault();
+        console.log(trigger);
+    }
+
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -14,13 +19,16 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="">Dashboard <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" onClick={(e) => this.processClick(e, 'dash')} href="#">Dashboard <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="">App Store</a>
+                    <a className="nav-link" onClick={(e) => this.processClick(e, 'products')} href="#">Products</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="">Profile</a>
+                    <a className="nav-link" onClick={(e) => this.processClick(e, 'orders')} href="#">Orders</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" onClick={(e) => this.processClick(e, 'reports')} href="#">Reports</a>
                 </li>
               </ul>
               <form className="form-inline my-2 my-lg-0">
