8d87436057d4104a5a1ed76d7fef99d5ebb7d795
David Green <pbcub1.dg@gmail.com>
2018-08-16 20:31:17 -0600

    Implementation of SideNavi
    Implemented SideNavi into App.js and built a small template in com_SideNavi.js. Refactored the tabbing and spacing in com_Navbar.js and got rid of a legacy import no longer used in App.js.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import $ from 'jquery';
 
 //Components
 import Navbar from './com_Navbar.js';
+import SideNavi from './com_SideNavi.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <SideNavi />
       </div>
     );
   }
---
+++ src/com_SideNavi.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SideNavi extends Component {
+    render(){
+        return (
+            <div className="sideNavi">
+                <div className="object"></div>
+                <div className="object"></div>
+            </div>
+        );
+    }
+}
