4aaa29cc9325310a0c68a3d88a1a1e6ed77d3c83
David Green <pbcub1.dg@gmail.com>
2018-08-22 14:13:08 -0600

    Cleared link warnings
    I cleared the href tags in the navbar so that the warnings would go away.

--- src/com_Navbar.js
+++ src/com_Navbar.js
@@ -29,16 +29,16 @@ class Navbar extends Component {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" onClick={(e) => this.processClick(e, 'dash')} href="#">Dashboard <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" onClick={(e) => this.processClick(e, 'dash')}>Dashboard <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" onClick={(e) => this.processClick(e, 'products')} href="#">Products</a>
+                        <a className="nav-link" onClick={(e) => this.processClick(e, 'products')}>Products</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" onClick={(e) => this.processClick(e, 'orders')} href="#">Orders</a>
+                        <a className="nav-link" onClick={(e) => this.processClick(e, 'orders')}>Orders</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" onClick={(e) => this.processClick(e, 'reports')} href="#">Reports</a>
+                        <a className="nav-link" onClick={(e) => this.processClick(e, 'reports')}>Reports</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
